8869fd74c3e768fa797d1f200001968e1cadc940
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2023-08-20 23:24:10 -0400

    Add project

---
+++ GHCommitGraphAnimator/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GHCommitGraphAnimator
+//
+//  Created by Campbell on 5/15/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
